54c4ddcc6421728082887f783222c1294f64b18a
touch4645 <k.terazawa@lillyholdings.co.jp>
2021-08-12 19:14:51 +0900

    recreate SoQL functions

--- src/SalesForce.js
+++ src/SalesForce.js
@@ -1,51 +1,84 @@
-var AUTHORIZATION_URL = `https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}`;
-var ACCESS_TOKEN_URL = "https://login.salesforce.com/services/oauth2/token";
-var CLIENT_ID = "input your client_id";
-var CLIENT_SECRET = "input your client_secret";
-var REDIRECT_URI = "https://script.google.com/macros/s/********************/usercallback";
+let prop = PropertiesService.getScriptProperties();
 
-/**
- * メニューの設定
- */
-function onOpen() {
-  SpreadsheetApp.getActiveSpreadsheet().addMenu("Salesforce Utility", [
-    {name:"OpenDialog", functionName: "openDialog"}
-  ]);
-}
+const CLIENT_ID =  prop.getProperty("CLIENT_ID");
+const CLIENT_SECRET =  prop.getProperty("CLIENT_SECRET");
+const USERNAME = prop.getProperty("USERNAME");
+const PASSWORD = prop.getProperty("PASSWORD");
 
-/**
- * ダイアログを開く
- */
-function openDialog() {
-  var prop = PropertiesService.getUserProperties();
-  var template = HtmlService.createTemplateFromFile("dialog");
-  var authorization_url = AUTHORIZATION_URL;
-  authorization_url = authorization_url.replace("{client_id}", CLIENT_ID);
-  authorization_url = authorization_url.replace("{redirect_uri}", encodeURIComponent(REDIRECT_URI));
-  authorization_url = authorization_url.replace("{state}", ScriptApp.newStateToken().withMethod("callback").withTimeout(120).createToken());
-  
-  template.authorization_url = authorization_url
-  SpreadsheetApp.getActive().show(template.evaluate());
-}
+const REDIRECT_URI = prop.getProperty("REDIRECT_URI");
 
-/**
- * OAuthでコールバックされたcode値を元にaccess_tokenを取得・保持する。
- */
-function callback(e) {
-  var code = e.parameter.code;
-  var res = UrlFetchApp.fetch(ACCESS_TOKEN_URL, {
-    "method" : "POST",
-    "payload" : {
-      "grant_type" : "authorization_code",
-      "code" : code,
-      "redirect_uri" : REDIRECT_URI,
-      "client_id" : CLIENT_ID,
-      "client_secret" : CLIENT_SECRET
+const ACCESS_TOKEN_URL = "https://login.salesforce.com/services/oauth2/token";
+
+
+// アクセストークン取得メソッド
+function authorization(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, username=USERNAME, password=PASSWORD) {
+  const response = UrlFetchApp.fetch(
+      ACCESS_TOKEN_URL, 
+      {
+        "method" : "POST",
+        "payload" : {
+          "grant_type": "password",
+          "client_id": client_id, //コンシューマ鍵
+          "client_secret": client_secret, //コンシューマの秘密
+          "username": username, //ユーザ名
+          "password": password //パスワード
+      },
+      "muteHttpExceptions": true
+    });
+    if (response.getResponseCode() == 200) {
+      return JSON.parse(response.getContentText());
+    } else {
+      throw new Error(response.getContentText());
     }
+}
+
+
+function query(q) {
+  q = encodeURIComponent(q).replace(/%20/g, '+');
+
+  const authInfo = authorization();
+  const response = UrlFetchApp.fetch(authInfo.instance_url + "/services/data/v35.0/query/?q=" + q, {
+    "method" : "GET",
+    "headers" : {
+      "Authorization": "Bearer " + authInfo.access_token
+    },
+    "muteHttpExceptions": true
   });
+  
+  const queryResult = JSON.parse(response.getContentText());
+  let result = queryResult.records;
 
-  var prop = PropertiesService.getUserProperties();
-  prop.setProperty("session_info", res.getContentText());
+  let nextRecordsUrl = queryResult.nextRecordsUrl;
 
-  return HtmlService.createHtmlOutput("<div>windowを閉じて!!</div>");
+  while(nextRecordsUrl) {
+    const nextResponse = UrlFetchApp.fetch(authInfo.instance_url + nextRecordsUrl, {
+      "method" : "GET",
+      "headers" : {
+        "Authorization": "Bearer " + authInfo.access_token
+      },
+      "muteHttpExceptions": true
+    });
+    const nextQueryResult = JSON.parse(nextResponse.getContentText());
+    result = result.concat(nextQueryResult.records);
+    nextRecordsUrl = nextQueryResult.nextRecordsUrl;
+  }
+  return result;
+}
+
+
+function getOpportunity() {
+  var records = query(
+    "SELECT Id, Amount, Name, CloseDate, LastModifiedDate"
+    + " FROM Opportunity"
+    + " WHERE NOT Name LIKE '%クール目%'"
+  );
+
+  for(var i = 0; i < records.length; i++) {
+    Logger.log({
+      id: records[i].Id, //商談ID
+      amount: records[i].Amount, //金額
+      name: records[i].Name, //商談名
+      closeDate: records[i].CloseDate //完了予定日
+    });
+  }
 }
